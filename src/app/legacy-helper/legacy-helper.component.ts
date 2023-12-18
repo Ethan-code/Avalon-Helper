1f1b75cce219b0644fc6c19220d828a5aaa826f2
Ethan <meliux30929@gmail.com>
2023-12-18 21:19:45 +0800

    refactor game result area code
    - TODO: background image should hide when no result

--- src/app/legacy-helper/legacy-helper.component.ts
+++ src/app/legacy-helper/legacy-helper.component.ts
@@ -44,11 +44,11 @@ export class LegacyHelperComponent implements AfterViewInit {
   private vote_html =
     "<div class='info'><label><input id='ticket' type='checkbox'/><div class='ticket'></div><label><input id='mission' type='checkbox'/><div class='mission'></div></label></label></div>";
 
-  private game1Max = 2;
-  private game2Max = 3;
-  private game3Max = 2;
-  private game4Max = 3;
-  private game5Max = 3;
+  public game1Max = 2;
+  public game2Max = 3;
+  public game3Max = 2;
+  public game4Max = 3;
+  public game5Max = 3;
 
   private left = 440;
 
@@ -56,11 +56,11 @@ export class LegacyHelperComponent implements AfterViewInit {
   private l2 = 0;
   private l3 = 0;
 
-  private a = 0;
-  private b = 0;
-  private c = 0;
-  private d = 0;
-  private e = 0;
+  public a = 0;
+  public b = 0;
+  public c = 0;
+  public d = 0;
+  public e = 0;
 
   public playerCount = 5;
   public get playerCounts(): number[] {
@@ -125,291 +125,146 @@ export class LegacyHelperComponent implements AfterViewInit {
   }
 
   public onGame1Click() {
-    var div = document.getElementById('game1');
-    if (div === null) return;
     if (this.a === 0) {
       this.a++;
-      div.classList.add('hideBefore');
-      div.style.backgroundImage =
-        "url('https://andyventure.com/wp-content/uploads/boardgame/avalon/marker_score.webp')";
-      div.style.backgroundColor = 'transparent';
-      div.style.backgroundRepeat = 'no-repeat';
-      div.style.backgroundSize = 'cover';
-      div.style.backgroundPosition = 'left';
-      $('.game1').attr('value', '');
+      $('#game1').attr('value', '');
     } else if (this.a === 1) {
-      div.style.backgroundPosition = 'right';
-      $('.game1').css('font-size', '20px');
-      $('.game1').css('color', 'white');
       this.a++;
     } else if (this.a === 2) {
-      div.style.backgroundPosition = 'right';
-      $('.game1').attr('value', '2');
-      $('.game1').css('font-size', '40px');
-      $('.game1').css('color', 'white');
       if (this.a === this.game1Max) {
         this.a = 0;
       } else {
         this.a++;
       }
     } else if (this.a === 3) {
-      div.style.backgroundPosition = 'right';
-      $('.game1').attr('value', '');
-      $('.game1').attr('value', '3');
-      $('.game1').css('font-size', '40px');
-      $('.game1').css('color', 'white');
       if (this.a === this.game1Max) {
         this.a = 0;
       } else {
         this.a++;
       }
     } else if (this.a === 4) {
-      div.style.backgroundPosition = 'right';
-      $('.game1').attr('value', '4');
-      $('.game1').css('font-size', '40px');
-      $('.game1').css('color', 'white');
       if (this.a === this.game1Max) {
         this.a = 0;
       } else {
         this.a++;
       }
     } else {
-      div.style.backgroundPosition = 'right';
-      $('.game1').attr('value', '5');
-      $('.game1').css('font-size', '40px');
-      $('.game1').css('color', 'white');
       this.a = 0;
     }
   }
 
   public onGame2Click() {
-    var div = document.getElementById('game2');
-    if (div === null) return;
     if (this.b === 0) {
       this.b++;
-      div.classList.add('hideBefore');
-      div.style.backgroundImage =
-        "url('https://andyventure.com/wp-content/uploads/boardgame/avalon/marker_score.webp')";
-      div.style.backgroundColor = 'transparent';
-      div.style.backgroundRepeat = 'no-repeat';
-      div.style.backgroundSize = 'cover';
-      div.style.backgroundPosition = 'left';
-      $('.game2').attr('value', '');
+      $('#game2').attr('value', '');
     } else if (this.b === 1) {
-      div.style.backgroundPosition = 'right';
-      $('.game2').css('font-size', '20px');
-      $('.game2').css('color', 'white');
       this.b++;
     } else if (this.b === 2) {
-      div.style.backgroundPosition = 'right';
-      $('.game2').attr('value', '2');
-      $('.game2').css('font-size', '40px');
-      $('.game2').css('color', 'white');
       if (this.b === this.game2Max) {
         this.b = 0;
       } else {
         this.b++;
       }
     } else if (this.b === 3) {
-      div.style.backgroundPosition = 'right';
-      $('.game2').attr('value', '');
-      $('.game2').attr('value', '3');
-      $('.game2').css('font-size', '40px');
-      $('.game2').css('color', 'white');
       if (this.b === this.game2Max) {
         this.b = 0;
       } else {
         this.b++;
       }
     } else if (this.b === 4) {
-      div.style.backgroundPosition = 'right';
-      $('.game2').attr('value', '4');
-      $('.game2').css('font-size', '40px');
-      $('.game2').css('color', 'white');
       if (this.b === this.game2Max) {
         this.b = 0;
       } else {
         this.b++;
       }
     } else {
-      div.style.backgroundPosition = 'right';
-      $('.game2').attr('value', '5');
-      $('.game2').css('font-size', '40px');
-      $('.game2').css('color', 'white');
       this.b = 0;
     }
   }
 
   public onGame3Click() {
-    var div = document.getElementById('game3');
-    if (div === null) return;
     if (this.c === 0) {
       this.c++;
-      div.classList.add('hideBefore');
-      div.style.backgroundImage =
-        "url('https://andyventure.com/wp-content/uploads/boardgame/avalon/marker_score.webp')";
-      div.style.backgroundColor = 'transparent';
-      div.style.backgroundRepeat = 'no-repeat';
-      div.style.backgroundSize = 'cover';
-      div.style.backgroundPosition = 'left';
-      $('.game3').attr('value', '');
+      $('#game3').attr('value', '');
     } else if (this.c === 1) {
-      div.style.backgroundPosition = 'right';
-      $('.game3').css('font-size', '20px');
-      $('.game3').css('color', 'white');
       this.c++;
     } else if (this.c === 2) {
-      div.style.backgroundPosition = 'right';
-      $('.game3').attr('value', '2');
-      $('.game3').css('font-size', '40px');
-      $('.game3').css('color', 'white');
       if (this.c === this.game3Max) {
         this.c = 0;
       } else {
         this.c++;
       }
     } else if (this.c === 3) {
-      div.style.backgroundPosition = 'right';
-      $('.game3').attr('value', '');
-      $('.game3').attr('value', '3');
-      $('.game3').css('font-size', '40px');
-      $('.game3').css('color', 'white');
       if (this.c === this.game3Max) {
         this.c = 0;
       } else {
         this.c++;
       }
     } else if (this.c === 4) {
-      div.style.backgroundPosition = 'right';
-      $('.game3').attr('value', '4');
-      $('.game3').css('font-size', '40px');
-      $('.game3').css('color', 'white');
       if (this.c === this.game3Max) {
         this.c = 0;
       } else {
         this.c++;
       }
     } else {
-      div.style.backgroundPosition = 'right';
-      $('.game3').attr('value', '5');
-      $('.game3').css('font-size', '40px');
-      $('.game3').css('color', 'white');
       this.c = 0;
     }
   }
 
   public onGame4Click() {
-    var div = document.getElementById('game4');
-    if (div === null) return;
     if (this.d === 0) {
       this.d++;
-      div.classList.add('hideBefore');
-      div.style.backgroundImage =
-        "url('https://andyventure.com/wp-content/uploads/boardgame/avalon/marker_score.webp')";
-      div.style.backgroundColor = 'transparent';
-      div.style.backgroundRepeat = 'no-repeat';
-      div.style.backgroundSize = 'cover';
-      div.style.backgroundPosition = 'left';
-      $('.game4').attr('value', '');
+      $('#game4').attr('value', '');
     } else if (this.d === 1) {
-      div.style.backgroundPosition = 'right';
-      $('.game4').css('font-size', '20px');
-      $('.game4').css('color', 'white');
       this.d++;
     } else if (this.d === 2) {
-      div.style.backgroundPosition = 'right';
-      $('.game4').attr('value', '2');
-      $('.game4').css('font-size', '40px');
-      $('.game4').css('color', 'white');
       if (this.d === this.game4Max) {
         this.d = 0;
       } else {
         this.d++;
       }
     } else if (this.d === 3) {
-      div.style.backgroundPosition = 'right';
-      $('.game4').attr('value', '');
-      $('.game4').attr('value', '3');
-      $('.game4').css('font-size', '40px');
-      $('.game4').css('color', 'white');
       if (this.d === this.game4Max) {
         this.d = 0;
       } else {
         this.d++;
       }
     } else if (this.d === 4) {
-      div.style.backgroundPosition = 'right';
-      $('.game4').attr('value', '4');
-      $('.game4').css('font-size', '40px');
-      $('.game4').css('color', 'white');
       if (this.d === this.game4Max) {
         this.d = 0;
       } else {
         this.d++;
       }
     } else {
-      div.style.backgroundPosition = 'right';
-      $('.game4').attr('value', '5');
-      $('.game4').css('font-size', '40px');
-      $('.game4').css('color', 'white');
       this.d = 0;
     }
   }
 
   public onGame5Click() {
-    var div = document.getElementById('game5');
-    if (div === null) return;
     if (this.e === 0) {
       this.e++;
-      div.classList.add('hideBefore');
-      div.style.backgroundImage =
-        "url('https://andyventure.com/wp-content/uploads/boardgame/avalon/marker_score.webp')";
-      div.style.backgroundColor = 'transparent';
-      div.style.backgroundRepeat = 'no-repeat';
-      div.style.backgroundSize = 'cover';
-      div.style.backgroundPosition = 'left';
-      $('.game5').attr('value', '');
+      $('#game5').attr('value', '');
     } else if (this.e === 1) {
-      div.style.backgroundPosition = 'right';
-      $('.game5').css('font-size', '20px');
-      $('.game5').css('color', 'white');
       this.e++;
     } else if (this.e === 2) {
-      div.style.backgroundPosition = 'right';
-      $('.game5').attr('value', '2');
-      $('.game5').css('font-size', '40px');
-      $('.game5').css('color', 'white');
       if (this.e === this.game5Max) {
         this.e = 0;
       } else {
         this.e++;
       }
     } else if (this.e === 3) {
-      div.style.backgroundPosition = 'right';
-      $('.game5').attr('value', '');
-      $('.game5').attr('value', '3');
-      $('.game5').css('font-size', '40px');
-      $('.game5').css('color', 'white');
       if (this.e === this.game5Max) {
         this.e = 0;
       } else {
         this.e++;
       }
     } else if (this.e === 4) {
-      div.style.backgroundPosition = 'right';
-      $('.game5').attr('value', '4');
-      $('.game5').css('font-size', '40px');
-      $('.game5').css('color', 'white');
       if (this.e === this.game5Max) {
         this.e = 0;
       } else {
         this.e++;
       }
     } else {
-      div.style.backgroundPosition = 'right';
-      $('.game5').attr('value', '5');
-      $('.game5').css('font-size', '40px');
-      $('.game5').css('color', 'white');
       this.e = 0;
     }
   }
@@ -490,11 +345,6 @@ export class LegacyHelperComponent implements AfterViewInit {
     if (this.roles.list.length == 6) {
       $('.game').css('font-size', '50px');
       $('.game').css('color', 'navy');
-      $('.game1').attr('value', '2');
-      $('.game2').attr('value', '3');
-      $('.game3').attr('value', '4');
-      $('.game4').attr('value', '3');
-      $('.game5').attr('value', '4');
       this.game1Max = 2;
       this.game2Max = 3;
       this.game3Max = 4;
@@ -503,11 +353,6 @@ export class LegacyHelperComponent implements AfterViewInit {
     } else if (this.roles.list.length == 7) {
       $('.game').css('font-size', '50px');
       $('.game').css('color', 'navy');
-      $('.game1').attr('value', '2');
-      $('.game2').attr('value', '3');
-      $('.game3').attr('value', '3');
-      $('.game4').attr('value', '4');
-      $('.game5').attr('value', '4');
       this.game1Max = 2;
       this.game2Max = 3;
       this.game3Max = 3;
@@ -516,11 +361,6 @@ export class LegacyHelperComponent implements AfterViewInit {
     } else if (this.roles.list.length >= 8) {
       $('.game').css('font-size', '50px');
       $('.game').css('color', 'navy');
-      $('.game1').attr('value', '3');
-      $('.game2').attr('value', '4');
-      $('.game3').attr('value', '4');
-      $('.game4').attr('value', '5');
-      $('.game5').attr('value', '5');
       this.game1Max = 3;
       this.game2Max = 4;
       this.game3Max = 4;
@@ -529,11 +369,6 @@ export class LegacyHelperComponent implements AfterViewInit {
     } else {
       $('.game').css('font-size', '50px');
       $('.game').css('color', 'navy');
-      $('.game1').attr('value', '2');
-      $('.game2').attr('value', '3');
-      $('.game3').attr('value', '2');
-      $('.game4').attr('value', '3');
-      $('.game5').attr('value', '3');
       this.game1Max = 2;
       this.game2Max = 3;
       this.game3Max = 2;
